e31121f906d73794239decc959c14c9736e0a4d7
FuuKowatty <miedzio.2000@gmail.com>
2023-05-29 13:52:19 +0200

    development: Starting hero-section

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import 'sass/style.scss';
 
 export function App() {
   return (
-    <div className='chuj'>
-      siema z rana
+    <div className='container'>
+      <div className='hero-section'>
+        Discover Thailand
+      </div>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,5 @@ import { App } from 'App'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-    <h1>siema</h1>
   </React.StrictMode>,
 )
