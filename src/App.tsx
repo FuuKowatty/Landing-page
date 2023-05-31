3dacf2eaa3eed81399ce17e6cebd13033dda681c
FuuKowatty <miedzio.2000@gmail.com>
2023-05-31 14:42:41 +0200

    Tours: Add explore section - beta

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Navbar } from 'components/Navbar'
 import { Header } from 'sections/Header'
+import { Explore } from 'sections/Explore'
 
 export function App() {
   return (
     <div className='container'>
       <Navbar />
       <Header />
+      <Explore />
     </div>
   )
 }
---
+++ src/components/TourCard.tsx
@@ -0,0 +1,24 @@
+import 'sass/style.scss';
+
+interface TourCardProps {
+    image: string;
+    name: string;
+    description: string;
+    price: number;
+}
+
+export function TourCard({ image, name, description, price } : TourCardProps) {
+  return (
+    <div className="tour-card">
+      <img className="tour-card__image" src={image} alt={name} />
+      <div className="tour-card__content">
+        <h3 className="tour-card__name">{name}</h3>
+        <p className="tour-card__description">{description}</p>
+      </div>
+      <div className="tour-card__price">Price: ${price}</div>
+      <button className='button--card'>Book Now</button>
+    </div>
+  );
+};
+
+
